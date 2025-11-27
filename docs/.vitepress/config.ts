062a71a7299a63ee0f02a9a25ab741447b12b44f
Roland Boon <roland@rolandboon.com>
2025-11-27 21:18:09 +0100

    Bump version to 0.8.1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,7 +19,7 @@ export default withMermaid(
         { text: 'Guide', link: '/guide/getting-started' },
         { text: 'API', link: '/api/' },
         {
-          text: 'v0.8.0',
+          text: 'v0.8.1',
           link: 'https://github.com/rolandboon/glasswork/releases',
         },
       ],
